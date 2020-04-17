0d2741114c552e49203cadab9e52d8319d4ea5bd
tomysshadow <ellescuba@gmail.com>
2020-04-17 01:56:03 -0600

    update to 1.4.7: fix page refreshes

--- Extender/main.cpp
+++ Extender/main.cpp
@@ -1,14 +1,21 @@
 #include <windows.h>
+#include <atlbase.h>
+#include <Shlwapi.h>
 #include "main.h"
 
-const wchar_t* SYSDIR_SHOCKWAVE = L"..\\..\\..\\BrowserPlugins\\Shockwave";
-const wchar_t* SYSDIR_CLICKTEAM = L"..\\..\\..\\BrowserPlugins\\Clickteam";
+const size_t MAX_PATH_WIDE = MAX_PATH * 2;
+const wchar_t* BROWSER_PLUGINS = L"\\..\\..\\..\\BrowserPlugins\\";
+const wchar_t* SHOCKWAVE = L"Shockwave";
+const wchar_t* CLICKTEAM = L"Clickteam";
+wchar_t sysdirShockwave[MAX_PATH_WIDE];
+wchar_t sysdirClickteam[MAX_PATH_WIDE];
 
 __declspec(naked) void getSystemDirectoryWExtendedCodeShockwave() {
 	__asm {
 		push eax;
 		push[esp + 0x0000000C];
-		push[SYSDIR_SHOCKWAVE];
+		lea eax, [sysdirShockwave];
+		push eax;
 		push[esp + 0x00000010];
 		call wcsncpy;
 		pop eax;
@@ -23,7 +30,8 @@ __declspec(naked) void getSystemDirectoryWExtendedCodeClickteam() {
 	__asm {
 		push eax;
 		push[esp + 0x0000000C];
-		push[SYSDIR_CLICKTEAM];
+		lea eax, [sysdirClickteam];
+		push eax;
 		push[esp + 0x00000010];
 		call wcsncpy;
 		pop eax;
@@ -37,11 +45,51 @@ __declspec(naked) void getSystemDirectoryWExtendedCodeClickteam() {
 bool extender() {
 	// set this to your Error Caption
 	LPCTSTR errorCaption = "Browser Plugin Extender Error";
+	char sysdir[MAX_PATH];
+
+	if (!GetModuleFileName(NULL, sysdir, MAX_PATH)) {
+		MessageBox(NULL, "Failed to Get Module Filename", errorCaption, MB_OK | MB_ICONERROR);
+		return false;
+	}
+	
+	while (!PathIsDirectory(sysdir)) {
+		if (!PathRemoveFileSpec(sysdir)) {
+			MessageBox(NULL, "Failed to Remove Path File Spec", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+	}
+
+	wchar_t sysdirFullPathName[MAX_PATH_WIDE];
 
 	// get Module Handle
 	HMODULE shockwaveModuleHandle = GetModuleHandle("np32dsw.dll");
 
 	if (shockwaveModuleHandle) {
+		if (wcsncpy_s(sysdirShockwave, CA2W(sysdir), MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Copy sysdirShockwave String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncat_s(sysdirShockwave, BROWSER_PLUGINS, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Concatenate sysdirShockwave String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncat_s(sysdirShockwave, SHOCKWAVE, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Concatenate sysdirShockwave String after Concatenating sysdirShockwave String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (!GetFullPathNameW(sysdirShockwave, MAX_PATH_WIDE - 2, sysdirFullPathName, NULL)) {
+			MessageBox(NULL, "Failed to Get sysdirShockwave Full Path Name", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncpy_s(sysdirShockwave, sysdirFullPathName, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Copy sysdirShockwave String after Getting sysdirShockwave Full Path Name", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
 		// test it
 		const size_t SHOCKWAVE_TEST_CODE_SIZE = 10;
 		unsigned char shockwaveTestCode[SHOCKWAVE_TEST_CODE_SIZE] = { 0x41, 0x00, 0x64, 0x00, 0x6F, 0x00, 0x62, 0x00, 0x65, 0x00 };
@@ -109,6 +157,31 @@ bool extender() {
 	HMODULE clickteamModuleHandle = GetModuleHandle("NpCnc32.dll");
 
 	if (clickteamModuleHandle) {
+		if (wcsncpy_s(sysdirClickteam, CA2W(sysdir), MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Copy sysdirClickteam String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncat_s(sysdirClickteam, BROWSER_PLUGINS, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Concatenate sysdirClickteam String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncat_s(sysdirClickteam, CLICKTEAM, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Concatenate sysdirClickteam String after Concatenating sysdirClickteam String", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (!GetFullPathNameW(sysdirClickteam, MAX_PATH_WIDE - 2, sysdirFullPathName, NULL)) {
+			MessageBox(NULL, "Failed to Get sysdirClickteam Full Path Name", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
+		if (wcsncpy_s(sysdirClickteam, sysdirFullPathName, MAX_PATH_WIDE)) {
+			MessageBox(NULL, "Failed to Copy sysdirClickteam String after Getting sysdirClickteam Full Path Name", errorCaption, MB_OK | MB_ICONERROR);
+			return false;
+		}
+
 		// test it
 		const size_t CLICKTEAM_TEST_CODE_SIZE = 10;
 		unsigned char clickteamTestCode[CLICKTEAM_TEST_CODE_SIZE] = { 0x83, 0xEC, 0x34, 0x53, 0x55, 0x56, 0x57, 0x50, 0xFF, 0x15 };
@@ -124,7 +197,7 @@ bool extender() {
 			return false;
 		}
 
-		if (!extendCode(errorCaption, clickteamModuleHandle, 0x0003041A)) {
+		if (!extendCode(errorCaption, clickteamModuleHandle, 0x0003041B)) {
 			MessageBox(NULL, "Failed to Extend Code", errorCaption, MB_OK | MB_ICONERROR);
 			return false;
 		}
